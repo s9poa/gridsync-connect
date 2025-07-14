54a960aa7786571386206393cddc7eb1356e9696
s9poa <s9poah@gmail.com>
2025-07-14 19:26:52 -0400

    Optimized Images

--- src/directory/Home.jsx
+++ src/directory/Home.jsx
@@ -31,10 +31,11 @@ function Home() {
         <div className={styles["target-items"]}>
           <div className={styles.gradient}></div>
           <Header className={styles.header} title="Home"/>
-          {activeIndex === 0 && <Hero img="/home/ac-mirage.webp" gameTitle ="Assassin's Creed Mirage" title="Discover a Tribute to the Original" des="Experience a mordern take on the iconic features and gameplay that have defined a franchise for 15 years." ctaLink="/" ctaText="Get it now" />}
+          
+          {activeIndex === 0 && <Hero img="/home/far-cry-6.webp" gameTitle ="Far Cry 6" title="Ready for Your Next Adventure?" des="Discover Fary Cry 6 and take down the tyrannical dictaor Anton Castillo." ctaLink="/" ctaText="Play now" />}
           {activeIndex === 1 && <Hero img="/home/ac-shadows.webp" gameTitle ="Assassin's Creed Shadows" title="Save 25% on Assassin's Creed Shadows" des="Plus, get an extra 20% off your cart when you redeem 100 GridSync Points!" ctaLink="/" ctaText="Get the game" />}
           {activeIndex === 2 && <Hero img="/home/r6.webp" gameTitle ="Rainbow Six Siege" title="Get free access to Siege X now!" des="Engage in intense 5v5 attack vs defense matches where elite strategy execution triumph." ctaLink="/" ctaText="Play now" />}
-          {activeIndex === 3 && <Hero img="/home/far-cry-6.webp" gameTitle ="Far Cry 6" title="Ready for Your Next Adventure?" des="Discover Fary Cry 6 and take down the tyrannical dictaor Anton Castillo." ctaLink="/" ctaText="Play now" />}
+          {activeIndex === 3 && <Hero img="/home/ac-mirage.webp" gameTitle ="Assassin's Creed Mirage" title="Discover a Tribute to the Original" des="Experience a mordern take on the iconic features and gameplay that have defined a franchise for 15 years." ctaLink="/" ctaText="Get it now" />}
           <div className={styles["carousel-tracker"]}>
             <button className={`${styles.dot} ${activeIndex === 0 ? styles.active : ""}`} onClick={() => handleManualSwitch(0)}></button>
             <button className={`${styles.dot} ${activeIndex === 1 ? styles.active : ""}`} onClick={() => handleManualSwitch(1)}></button>
@@ -44,10 +45,10 @@ function Home() {
         </div>
         <div className={styles.gradientSizingContainer}>
           <div className={styles["target-container"]}>
-            <HeroTargeter img="/home/ac-mirage.webp" gameTitle="Assassin's Creed Mirage" title="Discover a Tribute to the Original" onClick={() => handleManualSwitch(0)} className={activeIndex === 0 ? styles.active : ""} />
+            <HeroTargeter img="/home/far-cry-6.webp" title="Ready for Your Next Adventure?" onClick={() => handleManualSwitch(0)} className={activeIndex === 0 ? styles.active : ""} />
             <HeroTargeter img="/home/ac-shadows.webp" gameTitle ="Assassin's Creed Shadows" title="Save 25% on Assassin's Creed Shadows" onClick={() => handleManualSwitch(1)} className={activeIndex === 1 ? styles.active : ""} />
             <HeroTargeter img="/home/r6.webp" title="Get free access to Siege X now!" onClick={() => handleManualSwitch(2)} className={activeIndex === 2 ? styles.active : ""} />
-            <HeroTargeter img="/home/far-cry-6.webp" title="Ready for Your Next Adventure?" onClick={() => handleManualSwitch(3)} className={activeIndex === 3 ? styles.active : ""} />
+            <HeroTargeter img="/home/ac-mirage.webp" gameTitle="Assassin's Creed Mirage" title="Discover a Tribute to the Original" onClick={() => handleManualSwitch(3)} className={activeIndex === 3 ? styles.active : ""} />
           </div>
           <div className={styles.mainVisualGradient}></div>
         </div>
@@ -78,9 +79,9 @@ function Home() {
         <h2 className="main-content-section-title">Latest news <Link to="/store">View all</Link></h2>
         <div className="main-content-section-grid grid-4">
           <LandscapeBox img="/home/trackmania.webp" subTitle="Trackmania" title="Summer Update 2025: Trackmania turns 5" date="3 days ago" />
-          <LandscapeBox img="/home/r6-bundle.avif" subTitle="Rainbow Six Siege X" title="New R6 Share Pro Team Bundles Are Live" date="6 days ago" />
-          <LandscapeBox img="/home/r6-showdown.avif" subTitle="Rainbow Six Siege X" title="New Twitch Drops For Siege X's Showdown Event" date="6 days ago" />
-          <LandscapeBox img="/home/crew-june-24.avif" subTitle="The Crew Motorfest" title="This Week in The Crew - June 24" date="6 days ago" />
+          <LandscapeBox img="/home/r6-bundle.webp" subTitle="Rainbow Six Siege X" title="New R6 Share Pro Team Bundles Are Live" date="6 days ago" />
+          <LandscapeBox img="/home/r6-showdown.webp" subTitle="Rainbow Six Siege X" title="New Twitch Drops For Siege X's Showdown Event" date="6 days ago" />
+          <LandscapeBox img="/home/crew-june-24.webp" subTitle="The Crew Motorfest" title="This Week in The Crew - June 24" date="6 days ago" />
         </div>
       </section>
 

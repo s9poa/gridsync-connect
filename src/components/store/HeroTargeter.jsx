3e05c072ce35a9489276baac5498058c7635a5ac
s9poa <s9poah@gmail.com>
2025-08-13 21:56:36 -0400

    Updated The Design for The Hero Targeters

--- src/components/store/HeroTargeter.jsx
+++ src/components/store/HeroTargeter.jsx
@@ -4,9 +4,7 @@ function HeroTargeter({ img, title, onClick, className }) {
   return (
     <button className={`${styles.container} ${className || ""}`} onClick={onClick}>
       <img src={img} alt="" width="300.8" height="169.19" />
-      <div>
-        <h3>{title}</h3>
-      </div>
+      <h3>{title}</h3>
     </button>
   )
 }
